7651edb7ff82b228f0b9c6e4ec25e26a9f673bc2
c0d3h01 <c0d3h01@gmail.com>
2025-03-19 21:55:00 +0530

    chore: git.nix style

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -1,11 +1,9 @@
 { config, username, useremail, pkgs, ... }:
-
 {
   programs.git = {
     enable = true;
     userName = "${username}";
     userEmail = "${useremail}";
-
     extraConfig = {
       init.defaultBranch = "master";
       push.autoSetupRemote = true;
@@ -13,65 +11,45 @@
       color.ui = true;
       fetch.prune = true;
       push.default = "current";
-
-      # Helpful aliases
+      # Core helpful aliases
       alias = {
         st = "status";
         co = "checkout";
         ci = "commit";
         br = "branch";
         unstage = "reset HEAD --";
-        last = "log -1 HEAD";
-        visual = "!gitk";
         graph = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       };
     };
-
-    # Delta is a syntax-highlighting pager for git
+    # Delta for better diffs
     delta = {
       enable = true;
       options = {
         navigate = true;
         light = false;
         side-by-side = true;
-        line-numbers = true;
         syntax-theme = "ansi";
       };
     };
-
-    # Advanced git attributes
-    attributes = [
-      "*.pdf diff=pdf"
-      "*.html diff=html"
-      "*.md diff=markdown"
-    ];
-
-    # Enable git-ignore
+    # Common ignores
     ignores = [
       # General
       ".DS_Store"
       "Thumbs.db"
-
       # Editor files
       ".idea/"
       ".vscode/"
       "*.swp"
-      "*.swo"
-
       # Environment files
       ".env"
-      ".env.local"
       ".direnv/"
       ".envrc"
-
       # Build artifacts
       "dist/"
       "build/"
       "node_modules/"
       "__pycache__/"
       "*.pyc"
-      ".pytest_cache/"
     ];
   };
 }
-
