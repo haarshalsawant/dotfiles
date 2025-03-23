f6814598c55b9c131a3f89a497f2d076d0e5abaa
c0d3h01 <c0d3h01@gmail.com>
2025-03-23 19:41:19 +0530

    rm: remove obsolete Nix configuration for virtualization support

--- nix/apps/vm.nix
+++
@@ -1,8 +0,0 @@
-{ config, username, pkgs, ... }:
-
-{
-  # -*-[ Virtualisation ]-*-
-  environment.systemPackages = with pkgs; [ gnome-boxes ];
-  virtualisation.libvirtd.enable = true;
-  users.users.${username}.extraGroups = [ "libvirtd" ];
-}
