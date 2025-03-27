65c7ca182123c0527abaa0d8153d18a94510520d
c0d3h01 <c0d3h01@gmail.com>
2025-03-27 11:19:12 +0530

    upd: simplify neovim configuration by removing commented plugin entries

--- home/modules/neovim/default.nix
+++ home/modules/neovim/default.nix
@@ -1,41 +1,23 @@
-{ pkgs, ... }:
-
+{ config
+, pkgs
+, ...
+}:
 {
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
-
-    # Use the default Neovim configuration with a few must-have plugins
     defaultEditor = true;
-
-    # Use a pre-configured Neovim distribution
     plugins = with pkgs.vimPlugins; [
-      # LazyVim provides a great pre-configured Neovim setup
-      {
-        plugin = lazy-nvim;
-        type = "lua";
-        config = '''';
-      }
-
-      # Pre-configured Neovim setup (will handle all plugins and configuration)
-      {
-        plugin = LazyVim;
-        type = "lua";
-        config = '''';
-      }
-
-      # Install tokyonight theme
+      lazy-nvim
+      LazyVim
       tokyonight-nvim
     ];
-
-    # Minimal configuration to bootstrap the LazyVim setup
     extraLuaConfig = ''
       -- Bootstrap LazyVim
       require("lazy").setup({
         spec = {
           { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-          -- Add your additional plugins here if needed
         },
         defaults = {
           lazy = true,
