3622a3038e83050c163fb891c33621a3cec7785a
c0d3h01 <c0d3h01@gmail.com>
2025-04-07 13:15:47 +0530

    upd: zsh autosuggestions, syntax highlighting, and custom functions

--- home/modules/zshell/default.nix
+++ home/modules/zshell/default.nix
@@ -8,8 +8,8 @@
       enable = true;
       dotDir = ".config/zsh";
 
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
       enableCompletion = true;
       autocd = true;
 
@@ -20,7 +20,7 @@
         ignoreSpace = true;
         save = 15000;
         size = 15000;
-        path = "${config.xdg.dataHome}/zsh/history";
+        path = "$HOME/.local/share/zsh/history";
         share = true;
       };
 
@@ -55,32 +55,98 @@
         cl = "clear";
         x = "exit";
         v = "nvim";
+        # Additional useful aliases
+        ports = "netstat -tulpn";
+        path = "echo $PATH | tr ':' '\n'";
+        weather = "curl wttr.in";
+        ip = "ip --color=auto";
+        diff = "diff --color=auto";
+        ping = "ping -c 5";
+        mkdir = "mkdir -pv";
+        wget = "wget -c";
       };
 
       # Environment setup
       envExtra = ''
         export MANPAGER="nvim +Man!"
         export PATH="$HOME/.local/bin:$PATH"
+        export EDITOR="nvim"
+        export VISUAL="nvim"
+        export TERM="xterm-256color"
+        export LANG="en_US.UTF-8"
+        export LC_ALL="en_US.UTF-8"
       '';
 
       # Functional config
       initExtra = ''
+        # Improved completion
         zstyle ':completion:*' menu select
         zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+        zstyle ':completion:*' list-colors "$LS_COLORS"
+        zstyle ':completion:*' special-dirs true
+        zstyle ':completion:*' use-cache on
+        zstyle ':completion:*' cache-path "$HOME/.cache/zsh/.zcompcache"
 
         # Improved Vi mode
         bindkey -v
         bindkey '^?' backward-delete-char
         bindkey '^h' backward-delete-char
-        bindkey '^w' backward-kill-w
+        bindkey '^w' backward-kill-word
+        bindkey '^[[A' history-substring-search-up
+        bindkey '^[[B' history-substring-search-down
+        bindkey '^[[C' forward-char
+        bindkey '^[[D' backward-char
+
         # Directory stack
         setopt AUTO_PUSHD
         setopt PUSHD_IGNORE_DUPS
+        setopt PUSHD_SILENT
+        setopt EXTENDED_GLOB
+        setopt NO_BEEP
+        setopt AUTO_CD
+        setopt MULTIOS
+        setopt CORRECT
+        setopt CORRECT_ALL
+        setopt DVORAK
+        setopt NO_HUP
+        setopt IGNORE_EOF
+        setopt PRINT_EIGHT_BIT
+        setopt RC_QUOTES
+        setopt RM_STAR_SILENT
+        setopt SHORT_LOOPS
+        setopt NO_FLOW_CONTROL
 
-        if [ -n "command fzf-share" ]; then
+        # Load FZF
+        if [ -n "$(command -v fzf-share)" ]; then
           source "$(fzf-share)/key-bindings.zsh"
           source "$(fzf-share)/completion.zsh"
         fi
+
+        # Custom functions
+        function mkcd() {
+          mkdir -p "$1" && cd "$1"
+        }
+
+        function extract() {
+          if [ -f "$1" ] ; then
+            case "$1" in
+              *.tar.bz2)   tar xjf "$1"     ;;
+              *.tar.gz)    tar xzf "$1"     ;;
+              *.bz2)       bunzip2 "$1"     ;;
+              *.rar)       unrar x "$1"     ;;
+              *.gz)        gunzip "$1"      ;;
+              *.tar)       tar xf "$1"      ;;
+              *.tbz2)      tar xjf "$1"     ;;
+              *.tgz)       tar xzf "$1"     ;;
+              *.zip)       unzip "$1"       ;;
+              *.Z)         uncompress "$1"  ;;
+              *.7z)        7z x "$1"        ;;
+              *)          echo "'$1' cannot be extracted via extract()" ;;
+            esac
+          else
+            echo "'$1' is not a valid file"
+          fi
+        }
       '';
     };
 
@@ -124,104 +190,101 @@
     # Starship prompt
     starship = {
       enable = true;
-      settings =
-        let
-          darkgray = "242";
-        in
-        {
-          format = lib.concatStrings [
-            "$username"
-            "($hostname )"
-            "$directory"
-            "($git_branch )"
-            "($git_state )"
-            "($git_status )"
-            "($git_metrics )"
-            "($cmd_duration )"
-            "$line_break"
-            "($python )"
-            "($nix_shell )"
-            "($direnv )"
-            "$character"
-          ];
+      settings = {
+        add_newline = true;
+        
+        format = "$all";
 
-          directory = {
-            style = "blue";
-            read_only = " !";
-          };
-
-          character = {
-            success_symbol = "[❯](purple)";
-            error_symbol = "[❯](red)";
-            vicmd_symbol = "[❮](green)";
-          };
-
-          git_branch = {
-            format = "[$branch]($style)";
-            style = darkgray;
-          };
-
-          git_status = {
-            format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted )](218)($ahead_behind$stashed)]($style)";
-            style = "cyan";
-            conflicted = "​";
-            untracked = "​";
-            modified = "​";
-            staged = "​";
-            renamed = "​";
-            deleted = "​";
-            stashed = "≡";
-          };
-
-          git_state = {
-            format = "[$state( $progress_current/$progress_total)]($style)";
-            style = "dimmed";
-            rebase = "rebase";
-            merge = "merge";
-            revert = "revert";
-            cherry_pick = "pick";
-            bisect = "bisect";
-            am = "am";
-            am_or_rebase = "am/rebase";
-          };
-
-          git_metrics.disabled = false;
-
-          cmd_duration = {
-            format = "[$duration]($style)";
-            style = "yellow";
-          };
-
-          python = {
-            format = "[$virtualenv]($style)";
-            style = darkgray;
-          };
-
-          nix_shell = {
-            format = "❄️";
-            style = darkgray;
-            heuristic = true;
-          };
-
-          direnv = {
-            format = "[($loaded/$allowed)]($style)";
-            style = darkgray;
-            disabled = false;
-            loaded_msg = "";
-            allowed_msg = "";
-          };
-
-          username = {
-            format = "[$user]($style)";
-            style_root = "yellow italic";
-            style_user = darkgray;
-          };
-
-          hostname = {
-            format = "@[$hostname]($style)";
-            style = darkgray;
-          };
+        character = {
+          success_symbol = "[❯](bold green)";
+          error_symbol = "[❯](bold red)";
+          vicmd_symbol = "[❮](bold green)";
         };
+
+        directory = {
+          style = "bold blue";
+          read_only = " ";
+          truncation_length = 3;
+          truncation_symbol = "…/";
+        };
+
+        git_branch = {
+          format = "[$symbol$branch]($style) ";
+          style = "bold purple";
+          symbol = " ";
+        };
+
+        git_status = {
+          format = "([$all_status$ahead_behind]($style) )";
+          style = "bold red";
+          conflicted = "=";
+          ahead = "⇡$count";
+          behind = "⇣$count";
+          diverged = "⇕⇡$ahead_count⇣$behind_count";
+          untracked = "?";
+          stashed = "≡";
+          modified = "!";
+          staged = "+";
+          renamed = "»";
+          deleted = "✘";
+        };
+
+        git_state = {
+          format = "([$state( $progress_current/$progress_total)]($style)) ";
+          style = "bright-black";
+        };
+
+        cmd_duration = {
+          format = "[$duration]($style) ";
+          style = "yellow";
+          min_time = 2000;
+        };
+
+        nix_shell = {
+          format = "[$symbol$state]($style) ";
+          symbol = "❄️ ";
+          style = "bold blue";
+        };
+
+        memory_usage = {
+          format = "$symbol[$ram_pct]($style) ";
+          symbol = "󰍛 ";
+          style = "bold dimmed white";
+          threshold = 75;
+        };
+
+        status = {
+          format = "[$symbol$status]($style) ";
+          symbol = "✖";
+          success_symbol = "✓";
+          not_executable_symbol = "🚫";
+          not_found_symbol = "🔍";
+          sigint_symbol = "⚡";
+          signal_symbol = "⚡";
+          style = "bold red";
+          map_symbol = true;
+          disabled = false;
+        };
+
+        python = {
+          format = "[$symbol$version]($style) ";
+          style = "bold yellow";
+          symbol = "🐍 ";
+        };
+
+        username = {
+          format = "[$user]($style) ";
+          style_user = "bold dimmed blue";
+          style_root = "bold red";
+          show_always = false;
+        };
+
+        hostname = {
+          format = "[$hostname]($style) ";
+          style = "bold dimmed green";
+          ssh_only = true;
+        };
+      };
     };
   };
 }
