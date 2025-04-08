d6e9d97a0bb71c4d0bdb398f0317062c6f872892
c0d3h01 <c0d3h01@gmail.com>
2025-04-08 12:07:00 +0530

    upd: migrate SSH keys to a new secrets directory

--- keys.nix
+++
@@ -1,3 +0,0 @@
-{
-  c0d3h01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPMHEDTYxp5o3ZLpEg4fWjXZ/MctVco8R9qYVfE0tIn+ c0d3h01@gmail.com";
-}
---
+++ secrets/keys.nix
@@ -0,0 +1,3 @@
+{
+  c0d3h01 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHS9IIy0c+tn7CXviUwEgIzoJ4k4yAaiJ7ebgMZpwPuJ c0d3h01@gmail.com";
+}
