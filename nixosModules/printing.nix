971e0a077b7475dd0b9e4e6319907191d1957379
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-20 21:39:04 +0530

    chore(printing): cleanup codes, un...

--- nixosModules/printing.nix
+++ nixosModules/printing.nix
@@ -1,20 +1,13 @@
-{ pkgs, ... }:
-
 {
   services = {
     printing = {
       enable = true;
       openFirewall = true;
-      drivers = with pkgs; [
-        gutenprint
-        hplip
-      ];
     };
 
     # Avahi (mDNS)
     avahi = {
       enable = true;
-      nssmdns4 = true;
       openFirewall = true;
     };
   };
