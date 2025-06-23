ceddfb15fe765ed78f59416d7aebb86092adbe9b
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-23 01:19:40 +0000

    chore(tweaks): hdd shed -> mq-deadline

--- machines/installer/tweaks.nix
+++ machines/installer/tweaks.nix
@@ -26,7 +26,7 @@
     # NVMe SSD: none (noop)
     ACTION=="add|change", KERNEL=="nvme0n1", ATTR{queue/scheduler}="none"
     # HDD: bfq (good for HDD, or use 'mq-deadline' for lowest latency)
-    ACTION=="add|change", KERNEL=="sda", ATTR{queue/scheduler}="bfq"
+    ACTION=="add|change", KERNEL=="sda", ATTR{queue/scheduler}="mq-deadline"
 
     # USB autosuspend for power saving
     ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
