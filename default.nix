9f5ca8b262297300a2f8992fd1283dbf2b6543b1
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-07-11 16:32:16 +0530

    chore: builds

--- default.nix
+++ default.nix
@@ -1,13 +1,7 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
-
-pkgs.mkShell {
-  name = "Dotfiles Shell";
-  # packages = with pkgs; [ pkgs.nix ];
-  buildInputs = with pkgs; [
-    nix
+with import <nixpkgs> {};
+mkShell {
+  nativeBuildInputs = [
+    bashInteractive
+    nodePackages.prettier
   ];
-  # shellHook = ''
-  # '';
 }
